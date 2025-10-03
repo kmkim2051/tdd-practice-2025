e2d3e2e54c574f8f2733c24b9341ad92cee1e6bd
Kyungmin Kim <qaws2051@gmail.com>
2025-10-04 03:06:06 +0900

    [red] threadlocal 기반 세팅

--- src/test/java/com/example/tdd_practice/practice/PracticeApplicationTests.java
+++ src/test/java/com/example/tdd_practice/practice/PracticeApplicationTests.java
@@ -1,22 +1,24 @@
 package com.example.tdd_practice.practice;
 
-import org.assertj.core.api.Assertions;
+import com.example.tdd_practice.practice.annotation.TimeoutCheck;
+import com.example.tdd_practice.practice.controller.LoggingController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class PracticeApplicationTests {
+
+  @Autowired
+  private LoggingController loggingController;
 
   @Test
   void 컨트롤러에_TimeoutCheck_라는_애너테이션_달려있음() throws Exception {
@@ -24,4 +26,16 @@ class PracticeApplicationTests {
     assertThat(method.isAnnotationPresent(TimeoutCheck.class)).isTrue();
   }
 
+  @Test
+  void N초이상_걸리면_실패() {
+    assertThrows(RuntimeException.class, () -> loggingController.slowMethod(5000));
+    // req: N초 설정
+  }
+
+  @Test
+  void Aspect_타임아웃_ThreadLocal_세팅() {
+    TimeoutContext.setThreshold(5);
+    long threshold = TimeoutContext.getThreshold();
+    assertThat(threshold).isEqualTo(5L);
+  }
 }
