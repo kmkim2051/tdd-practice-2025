4954997cd6672c524313b9a3dc839f757376b8fa
Kyungmin Kim <qaws2051@gmail.com>
2025-10-04 02:37:14 +0900

    [refactor] 애너테이션 디렉터리로 분리

--- src/main/java/com/example/tdd_practice/practice/controller/LoggingController.java
+++ src/main/java/com/example/tdd_practice/practice/controller/LoggingController.java
@@ -1,5 +1,6 @@
-package com.example.tdd_practice.practice;
+package com.example.tdd_practice.practice.controller;
 
+import com.example.tdd_practice.practice.annotation.TimeoutCheck;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
