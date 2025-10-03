8810788ca23d03583164040b7009d8f68d73cb5c
Kyungmin Kim <qaws2051@gmail.com>
2025-10-04 02:04:50 +0900

    [red] 애너테이션 존재여부 체크

--- src/test/java/com/example/tdd_practice/practice/PracticeApplicationTests.java
+++ src/test/java/com/example/tdd_practice/practice/PracticeApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.tdd_practice.practice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PracticeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void 컨트롤러에_CheckOnly_라는_애너테이션_달려있음() throws Exception {
+    Method method = LoggingController.class.getMethod("hello");
+    assertThat(method.isAnnotationPresent(CheckOnly.class)).isTrue();
+  }
 
 }
