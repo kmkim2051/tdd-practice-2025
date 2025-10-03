c8ade647c395e44401e7c8abbea8f5d585abba04
Kyungmin Kim <qaws2051@gmail.com>
2025-10-04 03:32:37 +0900

    [green] aspect 구현

--- src/main/java/com/example/tdd_practice/practice/aspect/TimeoutAspect.java
+++ src/main/java/com/example/tdd_practice/practice/aspect/TimeoutAspect.java
@@ -1,11 +1,14 @@
 package com.example.tdd_practice.practice.aspect;
 
 import com.example.tdd_practice.practice.annotation.TimeoutCheck;
+import com.example.tdd_practice.practice.util.TimeoutContext;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 public class TimeoutAspect {
@@ -15,6 +18,15 @@ public class TimeoutAspect {
     Object result = joinPoint.proceed();
     long duration = System.currentTimeMillis() - start;
 
-    return null;
+    Long testThreshold = TimeoutContext.get();
+    Long threshold = (testThreshold == null) ? timeoutCheck.threshold() : testThreshold;
+
+    log.info("threshold: {}, duration: {}", threshold,duration);
+
+    if (duration >= threshold * 1000) {
+      throw new RuntimeException();
+    }
+
+    return result;
   }
 }
